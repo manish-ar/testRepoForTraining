c48dd4997cf73568ceaab2566cc98207912e4114
manish-ar <43330596+manish-ar@users.noreply.github.com>
2018-09-17 12:39:47 +0530

    Update first.py

--- first.py
+++ first.py
@@ -19,9 +19,10 @@ print("Sum of two numbers is {}".format(x+y))
 
 
 #creating pandas DataFrame
-import pandas as pd
+import pandas
+a=pandas.dataframe()
 
-data=pd.DataFrame.from_dict(data={'A':{0:1,1:2,2:3},
+data=pandas.DataFrame.from_dict(data={'A':{0:1,1:2,2:3},
                              'B':{0:3,1:4,2:5},
                              'C':{0:7,1:8,2:9}
                              })
